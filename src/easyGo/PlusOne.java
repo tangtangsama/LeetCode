de4810b01b1b3bdee03965c070260d8a499e11e4
sucre <1462339680@qq.com>
2019-07-22 17:48:51 +0800

    easyGo-66-PlusOne

---
+++ src/easyGo/PlusOne.java
@@ -0,0 +1,26 @@
+package easyGo;
+
+/**
+ * @author sucre
+ * @date 2019-07-22
+ * @time 17:02
+ * @description https://leetcode-cn.com/problems/plus-one/
+ */
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {9,8,7,6,5,4,3,2,1,0};
+        plusOne(digits);
+    }
+
+    public static int[]plusOne(int[] digits) {
+        for(int i =digits.length-1;i>=0;i--)
+        {
+            digits[i]++;
+            digits[i] = digits[i]%10;
+            if(digits[i]!=0) return digits;
+        }
+        digits = new int[digits.length+1];
+        digits[0]=1;
+        return digits;
+    }
+}
